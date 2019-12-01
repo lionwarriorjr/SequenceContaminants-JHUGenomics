4c88fca46b0195015834a753f7cd3bb60288715b
clin113jhu <clin113@jhu.edu>
2019-12-01 16:40:58 -0500

    update bloomF and query_bf

--- python_scripts/bloomF.py
+++ python_scripts/bloomF.py
@@ -1,6 +1,7 @@
 import pybloom_live
 import pickle
 import argparse
+import os
 from tqdm import tqdm
 
 
@@ -28,37 +29,48 @@ def load_data(filename):
 
 def main():
     parse = argparse.ArgumentParser(description='Build Naive Bloom Filter')
-    parse.add_argument('-i', nargs='+', help='Input reference fasta')
-    parse.add_argument('-o', nargs='+', help='Output bloom filter path')
+    parse.add_argument('-i', help='Input reference fasta directory')
+    parse.add_argument('-o', help='Output bloom filter directory')
     args = parse.parse_args()
-    if len(args.i) != len(args.o):
-        parse.error('Numbers of Input and Output do not match')
-    for ip, op in zip(args.i, args.o):
-        seq_file = ip
+    SPECIES = {'GCF_000005845.2_ASM584v2_genomic.fna': 'e_coli_bf.txt',
+               'GCF_000072485.1_ASM7248v1_genomic.fna': 's_maltophilia_bf.txt',
+               'GCF_001457475.1_NCTC10807_genomic.fna': 'a_xylosoxidans_bf.txt',
+               'GCF_000018665.1_ASM1866v1_genomic.fna': 'd_acidovorans_bf.txt',
+               'GCF_000008345.1_ASM834v1_genomic.fna': 'p_acnes_bf.txt',
+               'GCF_000007645.1_ASM764v1_genomic.fna': 's_epidermidis_bf.txt',
+               'GCF_000746645.1_ASM74664v1_genomic.fna': 'a_baumannii_bf.txt',
+               'GCF_000146045.2_R64_genomic.fna': 's_cerevisiae_bf.txt',
+               'GCF_001661255.1_Wican1_genomic.fna': 'w_anomalus_bf.txt',
+               'GCF_003290485.1_ASM329048v1_genomic.fna': 'm_restricta_bf.txt',
+               'GCA_000710275.1_ASM71027v1_genomic.fna': 'p_notatum_bf.txt'}
+
+    for ip, op in SPECIES.items():
+        seq_file = os.path.join(args.i, ip)
         with open(seq_file, 'r') as f:
             seq = ''
-            # l = 0
+            l = 0
             for line in f:
-                # if l > 1000:
-                #     break
+                if l > 1000:
+                    break
                 if line.startswith('>'):
                     continue
                 seq = seq + line.rstrip()
-                # l += 1
+                l += 1
         kms = kmers(seq, 20)
-        print(ip)
+        print(op)
         print('number of kmers', len(kms))
-        sbf = pybloom_live.ScalableBloomFilter(mode=pybloom_live.ScalableBloomFilter.LARGE_SET_GROWTH)
+        sbf = pybloom_live.BloomFilter(capacity=2000000000, error_rate=0.008)
 
         for i, x in zip(kms, tqdm(range(len(kms)))):
             _ = sbf.add(i)
 
         if next(iter(kms)) in sbf:
             print('kms in sbf')
-        with open(op, 'wb') as fh:
+        out_file = os.path.join(args.o, op)
+        with open(out_file, 'wb') as fh:
             sbf.tofile(fh)
-        with open(op, 'rb') as b:
-            sbf_file = pybloom_live.ScalableBloomFilter.fromfile(b)
+        with open(out_file, 'rb') as b:
+            sbf_file = pybloom_live.BloomFilter.fromfile(b)
         if next(iter(kms)) in sbf_file:
             print('kms in file')
 
--- python_scripts/query_bf.py
+++ python_scripts/query_bf.py
@@ -1,10 +1,11 @@
 import sys
+import os
 import pybloom_live
 import argparse
 import numpy as np
 from tqdm import tqdm
 
-
+# Code from genome-sketching
 def kmers(seq, k):
     kms = set()
     num_k = len(seq) - k + 1
@@ -13,7 +14,7 @@ def kmers(seq, k):
         kms.add(km)
     return kms
 
-
+# Code from lecture
 def parse_fastq(fh):
     """ Parse reads from a FASTQ filehandle.  For each read, we
         return a name, nucleotide-string, quality-string triple. """
@@ -32,31 +33,62 @@ def parse_fastq(fh):
     return reads
 
 
+def dir_path(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        raise NotADirectoryError(string)
+
+
 def main():
     parse = argparse.ArgumentParser(description='Query Naive Bloom Filter')
-    parse.add_argument('-ibf', nargs='+', help='Input reference bloom filters')
+    parse.add_argument('-bf', type=dir_path, help='Input reference bloom filters directory')
     parse.add_argument('-f', help='Input query fastq')
     args = parse.parse_args()
 
     reads_kms = []
+    bf_files = ['e_coli_bf.txt', 's_maltophilia_bf.txt', 'a_xylosoxidans_bf.txt',
+                'd_acidovorans_bf.txt', 'p_acnes_bf.txt', 's_epidermidis_bf.txt',
+                'a_baumannii_bf.txt', 's_cerevisiae_bf.txt', 'w_anomalus_bf.txt',
+                'm_restricta_bf.txt', 'p_notatum_bf.txt']
+    bf_path = [os.path.join(args.bf, p) for p in bf_files]
     with open(args.f) as f:
         fq = parse_fastq(f)
     for read in fq:
         reads_kms.append(kmers(read, 20))
 
-    output = np.zeros((len(fq), len(args.ibf)))
-    for j, bf in enumerate(args.ibf):
+    output = np.zeros((len(fq), len(bf_path)), dtype=int)
+    for j, bf in enumerate(bf_path):
         with open(bf, 'rb') as b:
             sbf_file = pybloom_live.ScalableBloomFilter.fromfile(b)
             for i, read_k in enumerate(reads_kms):
-                output[i][j] = 1
+                read_kmer_len = len(read_k)
+                mis = 0
+                match = 0
                 for k in read_k:
-                    if k not in sbf_file:
-                        output[i][j] = 0
-                        break
+                    # if k in sbf_file:
+                    #     output[i][j] = 1
+                    #     break
+                    if k in sbf_file:
+                        match += 1
+                        if match/read_kmer_len >= 0.2:
+                            output[i][j] = 1
+                            break
+                        # if mis/read_kmer_len > 0.5:
+                        #     break
+                # if mis/read_kmer_len < 0.8:
+                #     output[i][j] = 1
 
-    for out in output:
-        sys.stdout.write(' '.join([str(x) for x in out]) + '\n')
+    score = np.zeros(len(bf_files), dtype=int)
+    x, y = np.nonzero(output)
+    for r in y:
+        score[r] += 1
+
+    # for out in output:
+    #     sys.stdout.write(' '.join([str(x) for x in out]) + '\n')
+
+    for b, s in zip(bf_files, score):
+        print(str(b) + ': ' + str(s))
 
 
 if __name__ == '__main__':
