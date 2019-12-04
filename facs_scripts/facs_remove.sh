3c06f995f53e5f3155811c7d608588a1acc97c75
clin113jhu <clin113@jhu.edu>
2019-12-03 22:43:24 -0500

    fix facs_remove.sh

--- facs_scripts/facs_remove.sh
+++ facs_scripts/facs_remove.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for filename in build/facs_build/facs_bf/*.bloom; do
   echo "$filename"
-  (./facs remove -r "$filename" -q "unmapped_mix3species.fastq") 2> "build/facs_build/contaminated_fastq/$(basename "$filename" .fna).fastq"
+  (./build/facs_build/facs/facs remove -r "$filename" -q "data/simulation/unmapped_mix3species.fastq") 2> "build/facs_build/contaminated_fastq/$(basename "$filename" .fna).fastq"
 done
