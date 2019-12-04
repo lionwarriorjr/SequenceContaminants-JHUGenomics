e5451dc3b9484f231bbf8c236d43275e2d2f5c45
Srihari Mohan <srihari_mohan@comcast.net>
2019-12-04 11:01:20 -0500

    Updating with final pipeline script

--- decontaminate.py
+++ decontaminate.py
@@ -1,3 +1,5 @@
+import os
+import csv
 import sys
 import numpy as np
 import random
@@ -11,8 +13,8 @@ import time
 import math
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-from dna2vec.dna2vec.multi_k_model import MultiKModel
 from collections import Counter, defaultdict
+from dna2vec.dna2vec.multi_k_model import MultiKModel
 
 
 # Constants for Simulation
@@ -30,15 +32,14 @@ OVERLAP_HEADER = ">Overlapped from Contaminant Genomes"
 OVERLAP_OUTPUT_FILE = "overlap.fna"
 
 # Constants for Bowtie2 alignment
-BOWTIE_ALIGNMENTS_INPUT = "cont.sam"
+UNMAPPED_OUTPUT_FILE = "unmapped.fastq"
+MAPPED_OUTPUT_FILE = "mapped.fastq"
 TABLE_LEN = 13
 UNMAPPED_STATUS_COL = 1
 UNMAPPED_FLAG = 4
 MAPPED_FLAG = 0
 NAME_COL = 0
 READ_COL = 9
-# how many of unmapped reads should be sampled for verification with Bowtie
-ALIGNMENT_VALIDATION_LIMIT = 100
 # initialize names of pre-built Bowtie indexes in current directory
 indexes = [
     'coli',
@@ -56,6 +57,8 @@ indexes = [
 
 # Constants for RNN evaluation
 
+# path to trained DL model
+MODEL_PATH = "rnn.pt"
 # threshold probability of max target class in RNN prediction
 RNN_THRESHOLD = 0.5
 # identities of contaminant sources with pre-built Bowtie indexes
@@ -106,7 +109,29 @@ class RNN(nn.Module):
         return torch.zeros(1, self.hidden_size)
 
 # load trained RNN model from disk
-rnn = torch.load('RNN_balanced.trained')
+rnn = torch.load(MODEL_PATH)
+
+# Constants for Bloom Filter
+
+# path to written Bloom Filter fastq files
+CON_PATH = './build/facs_build/contaminated_fastq'
+# species map from Bloom filter name to species id
+BLOOM = {
+    'e_coli.bloom.fastq': 0,
+    's_maltophilia.bloom.fastq': 1,
+    'a_xylosoxidans.bloom.fastq': 2,
+    'd_acidovorans.bloom.fastq': 3,
+    'p_acne.bloom.fastq': 4,
+    's_epidermidis.bloom.fastq': 5,
+    'a_baumannii.bloom.fastq': 6,
+    's_cerevisiae.bloom.fastq': 7,
+    'w_anomalus.bloom.fastq': 8,
+    'm_restricta.bloom.fastq': 9,
+    'p_notatum.bloom.fastq': 10
+}
+# output file for Bloom Filter predictions
+FACS_OUTPUT = "facs.tsv"
+
 
 # read fasta file into list of reads in-memory
 def parse_fasta(filename, read_length, limit=-1):
@@ -148,6 +173,26 @@ def parse_fasta(filename, read_length, limit=-1):
     return reads
 
 
+def parse_fastq(fh):
+    """ Parse reads from a FASTQ filehandle.  For each read, we
+        return a name, nucleotide-string, quality-string triple. """
+    names = []
+    reads = []
+    q_score = []
+    while True:
+        first_line = fh.readline()
+        if len(first_line) == 0:
+            break  # end of file
+        name = first_line[1:].rstrip()
+        names.append(name)
+        seq = fh.readline().rstrip()
+        fh.readline()  # ignore line starting with +
+        qual = fh.readline().rstrip()
+        reads.append(seq)
+        q_score.append(qual)
+    return reads, names
+
+
 # write reads to fasta file on disk 
 def write_fasta(filename, reads, names):
     with open(filename, "w") as fw:
@@ -164,6 +209,35 @@ def contaminate(src_reads, contaminant):
     src_reads_copy = src_reads.copy()
     src_reads_copy.extend(contaminant_reads)
     return src_reads_copy
+
+
+def write_facs_output():
+    output = {}
+    for f in os.listdir(CON_PATH):
+        con_file_path = os.path.join(CON_PATH, f)
+        if os.path.getsize(con_file_path) and f.endswith('fastq'):
+            species_name = BLOOM[f]
+            with open(con_file_path, 'r') as f:
+                reads, names = parse_fastq(f)
+                for r, n in zip(reads, names):
+                    if n in output:
+                        output[n][1].append(species_name)
+                    else:
+                        output[n] = [r, [species_name]]
+
+    with open("temp/" + FACS_OUTPUT, 'wt') as t:
+        tsv_writer = csv.writer(t, delimiter='\t')
+        for na, re in output.items():
+            if len(re[1]) == 1:
+                tsv_writer.writerow([na, re[0], re[1][0]])
+            else:
+                sn = ''
+                for i in re[1]:
+                    if sn:
+                        sn = str(sn) + ',' + str(i)
+                    else:
+                        sn = str(i)
+                tsv_writer.writerow([na, re[0], sn])
 
 
 # apply word embeddings to DNA reads
@@ -213,13 +287,13 @@ def main():
     # how many of the unmapped reads should be verified by Bowtie2
     n_args = len(sys.argv)-1
     # optional third command-line argument
-    alignment_limit = sys.argv[3] if n_args == 3 else ALIGNMENT_VALIDATION_LIMIT
+    final_output = sys.argv[3]
     
     # run bowtie alignment to find mapped/unmapped reads
     process = subprocess.Popen( args=["bowtie2",
                                 "-x", index,
                                 "-U", contaminated_file,
-                                "--un", BOWTIE_ALIGNMENTS_INPUT ],
+                                "--un", "temp/" + UNMAPPED_OUTPUT_FILE ],
                                 stdout=DEVNULL, stderr=STDOUT )
     # block until bowtie subprocess completes for initial alignment
     process.wait()
@@ -228,7 +302,7 @@ def main():
     print("Logging unmapped reads from Bowtie2 alignment of contaminated genome")
     unmapped_reads = []
     extract_read = False
-    with open(BOWTIE_ALIGNMENTS_INPUT, 'r') as fr:
+    with open("temp/" + UNMAPPED_OUTPUT_FILE, 'r') as fr:
         for line in fr:
             if extract_read:
                 unmapped_reads[len(unmapped_reads)-1][1] = line.strip()
@@ -243,62 +317,84 @@ def main():
 
     # forward propagate through RNN to get predictions for each read
     rnn_pred = {}
-    count = 1
     for (name, unmapped_read) in unmapped_reads:
         # run the unmapped read through the RNN
         if 'N' not in unmapped_read:
-            count += 1
             results = predict(unmapped_read)
             categories = [ret[0] for ret in results]
             probabilities = [ret[1] for ret in results]
             rnn_pred[name] = [unmapped_read, categories, probabilities]
 
     # get Bloom Filter predictions
-    
+    process = subprocess.Popen( args = ["./facs_remove.sh"], stdout=DEVNULL, stderr=STDOUT )
+    process.wait()
+    # write sketching predictions to current directory
+    write_facs_output()
+    overlapped_reads = rnn_pred
+    with open("temp/" + FACS_OUTPUT, 'r') as fr:
+        for line in fr:
+            values = line.split('\t')
+            name, text, categories = values[0], values[1], values[2]
+            categories = categories.strip().split(',')
+            categories = [int(c) for c in categories]
+            if name in overlapped_reads:
+                both = set(rnn_pred[name][1]).intersection(categories)
+                overlap_indices = [rnn_pred[name][1].index(x) for x in both]
+                overlap_categories, overlap_probs = [], []
+                for i in overlap_indices:
+                    overlap_categories.append(rnn_pred[name][1][i])
+                    overlap_probs.append(rnn_pred[name][2][i])
+                for category in categories:
+                    if category not in both:
+                        overlap_categories.append(category)
+                overlapped_reads[name][1] = overlap_categories
+                overlapped_reads[name][2] = overlap_probs
 
     # overlap classifications from DL and Bloom Filter are our predictions
-    overlapped_reads = rnn_pred
     print('predicted set of unmapped reads mapped to contaminant genomes:')
     class_preds = [list() for i in range(n_categories)]
     class_names = [list() for i in range(n_categories)]
-    for name in overlapped_reads:
-        results = overlapped_reads[name]
-        text, categories, probabilities = results[0], results[1], results[2]
-        predicted = []
-        for category in categories:
-            predicted.append(all_categories[category])
-            class_names[category].append(name)
-            class_preds[category].append(text)
-        print('read %s introduced by contaminant genome(s) %s with probabilities %s\n'%(text, str(predicted), str(probabilities)))
+    with open(final_output, 'w') as fw:
+        for name in overlapped_reads:
+            results = overlapped_reads[name]
+            text, categories, probabilities = results[0], results[1], results[2]
+            for i in range(len(categories)-len(probabilities)):
+                probabilities.append('none')
+            predicted = []
+            for category in categories:
+                predicted.append(all_categories[category])
+                class_names[category].append(name)
+                class_preds[category].append(text)
+            write_pred, write_prob = str(predicted), str(probabilities)
+            print('read %s introduced by contaminant genome(s) %s with probabilities %s\n'%(name, write_pred, write_prob))
+            fw.write("%s\t%s\t%s\t%s\n"%(name, text, write_pred, write_prob))
 
     for i in range(n_categories):
-        if len(class_preds[i]) > ALIGNMENT_VALIDATION_LIMIT:
-            write_fasta(filename=indexes[i] + ".fna", 
-                        reads=class_preds[i],
-                        names=class_names[i])
+        write_fasta(filename="temp/" + indexes[i] + ".fna", 
+                    reads=class_preds[i],
+                    names=class_names[i])
     
-    # sample verification of first ALIGNMENT_VALIDATION_LIMIT unmapped reads
+    # verify claimed reads from overlap analysis with Bowtie2
     print("Verifying unmapped reads against contaminant genome with Bowtie2 alignment")
+    reads = set()
     for i in range(n_categories):
-        if len(class_preds[i]) > ALIGNMENT_VALIDATION_LIMIT:
+        if len(class_preds[i]) > 0:
             # run the verification alignment
-            process = subprocess.Popen( args=["bowtie2", 
-                                        "-x", indexes[i], 
-                                        "-f", indexes[i] + ".fna", 
-                                        "-S", BOWTIE_ALIGNMENTS_INPUT ],
+            process = subprocess.Popen( args=["bowtie2",
+                                        "-x", "indexes/" + indexes[i],
+                                        "-f", "temp/" + indexes[i] + ".fna",
+                                        "--al", "temp/" + MAPPED_OUTPUT_FILE],
                                         stdout=DEVNULL, stderr=STDOUT )
             process.wait()
-            matched = total = 0
-            with open(BOWTIE_ALIGNMENTS_INPUT, 'r') as fr:
+            extract_read = False
+            with open("temp/" + MAPPED_OUTPUT_FILE, 'r') as fr:
                 for line in fr:
-                    values = line.split("\t")
-                    if len(values) > READ_COL:
-                        total += 1
-                        # check if alignment is a match against the contaminant genome
-                        if int(values[UNMAPPED_STATUS_COL]) == MAPPED_FLAG:
-                            matched += 1
-                            print('%s mapped to %s'%(values[READ_COL], all_categories[i]))
-            print("%d/%d of the unmapped reads predicted to come from %s actually came from %s"%(matched, total, all_categories[i], all_categories[i]))
+                    if extract_read:
+                        print('%s mapped to contaminant genome %s'%(line.strip(), all_categories[i]))
+                    extract_read = line.startswith(">")
+                    if extract_read:
+                        reads.add(line[1:].strip())
+    print("Summary report: %d/%d of discovered unmapped reads were successfully mapped to a contaminant"%(len(reads), len(unmapped_reads))) 
 
 if __name__ == "__main__":
     main()
